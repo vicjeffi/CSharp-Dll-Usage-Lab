9019c53522163c45edaa462fec9dc45220f285ba
Michael <monahovmm17@gmail.com>
2023-11-21 01:17:33 +0300

    1

---
+++ DllLabWork/DllLabWork/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace DllLabWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.red_textBox = new System.Windows.Forms.TextBox();
+            this.green_textBox = new System.Windows.Forms.TextBox();
+            this.blue_textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя ПК: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Имя пользователя: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Системная директория: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Версия ОС: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Тип загрузки: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 137);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Разширение монитора: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 163);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(177, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Путь к картинке рабочего стола: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 190);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Контрастность: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 220);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(119, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Цвет рабочего стола: ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(13, 248);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(39, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Дата: ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(13, 276);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(119, 13);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Цвет рабочего стола: ";
+            // 
+            // red_textBox
+            // 
+            this.red_textBox.Location = new System.Drawing.Point(127, 273);
+            this.red_textBox.Name = "red_textBox";
+            this.red_textBox.Size = new System.Drawing.Size(40, 20);
+            this.red_textBox.TabIndex = 11;
+            // 
+            // green_textBox
+            // 
+            this.green_textBox.Location = new System.Drawing.Point(173, 273);
+            this.green_textBox.Name = "green_textBox";
+            this.green_textBox.Size = new System.Drawing.Size(40, 20);
+            this.green_textBox.TabIndex = 12;
+            // 
+            // blue_textBox
+            // 
+            this.blue_textBox.Location = new System.Drawing.Point(219, 273);
+            this.blue_textBox.Name = "blue_textBox";
+            this.blue_textBox.Size = new System.Drawing.Size(40, 20);
+            this.blue_textBox.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Задать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(324, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Определить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 339);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.blue_textBox);
+            this.Controls.Add(this.green_textBox);
+            this.Controls.Add(this.red_textBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(376, 378);
+            this.MinimumSize = new System.Drawing.Size(376, 378);
+            this.Name = "Form1";
+            this.Text = "Знакомство с WINAPI Монахов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox red_textBox;
+        private System.Windows.Forms.TextBox green_textBox;
+        private System.Windows.Forms.TextBox blue_textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DllLabWork/DllLabWork/Form1.cs
@@ -0,0 +1,251 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DllLabWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //
+        // DLL Stuff
+        //
+
+        [DllImport("kernel32.dll")]
+        public static extern bool GetComputerName(
+                [Out] char[] lpBuffer,
+                ref uint lpnSize
+            );
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool GetUserName(
+        StringBuilder lpBuffer,
+        ref uint lpnSize);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern uint GetSystemDirectory(
+        StringBuilder lpBuffer,
+        uint uSize);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct OSVERSIONINFOEX
+        {
+            public int dwOSVersionInfoSize;
+            public int dwMajorVersion;
+            public int dwMinorVersion;
+            public int dwBuildNumber;
+            public int dwPlatformId;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public string szCSDVersion;
+            public short wServicePackMajor;
+            public short wServicePackMinor;
+            public short wSuiteMask;
+            public byte wProductType;
+            public byte wReserved;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetVersionEx(ref OSVERSIONINFOEX lpVersionInfo);
+
+        public const int SM_CLEANBOOT = 67;
+
+        [DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(int nIndex);
+
+        public const uint SPI_GETCONTRAST = 0x0014;
+        const uint SPI_SETDESKWALLPAPER = 0x0014;
+        const uint SPIF_UPDATEINIFILE = 0x01;
+        const uint SPIF_SENDCHANGE = 0x02;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SystemParametersInfo(
+            uint uiAction,
+            uint uiParam,
+            ref int pvParam,
+            uint fWinIni
+        );
+
+        public static bool GetSystemContrast(out int contrast)
+        {
+            contrast = 0;
+            return SystemParametersInfo(SPI_GETCONTRAST, 0, ref contrast, 0);
+        }
+        public static bool SetDesktopColor(int color)
+        {
+            // Используем SystemParametersInfo для установки цвета фона рабочего стола
+            return SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, ref color, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+        }
+
+        public static int RGB(byte red, byte green, byte blue)
+        {
+            return red | (green << 8) | (blue << 16);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SYSTEMTIME
+        {
+            public ushort wYear;
+            public ushort wMonth;
+            public ushort wDayOfWeek;
+            public ushort wDay;
+            public ushort wHour;
+            public ushort wMinute;
+            public ushort wSecond;
+            public ushort wMilliseconds;
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern void GetSystemTime(ref SYSTEMTIME lpSystemTime);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte red, green, blue;
+
+            try
+            {
+                red = byte.Parse(red_textBox.Text);
+                green = byte.Parse(green_textBox.Text);
+                blue = byte.Parse(blue_textBox.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Неверный формат цвета!");
+                return;
+            }
+
+            int color = RGB(red, green, blue);
+            SetDesktopColor(color);
+        }
+        private string GetPathOfWallpaper()
+        {
+            string pathWallpaper = "";
+            RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", false);
+            if (regKey != null)
+            {
+                pathWallpaper = regKey.GetValue("WallPaper").ToString();
+                regKey.Close();
+            }
+            return pathWallpaper;
+        }
+        const uint SPI_GETDESKWALLPAPER = 0x0073;
+
+        const int COLOR_BACKGROUND = 1;
+
+        [DllImport("user32.dll")]
+        public static extern int GetSysColor(int nIndex);
+        private void button2_Click(object sender, EventArgs e)
+        {
+            uint size = 256; // Максимальная длина имени компьютера
+            char[] buffer = new char[size];
+
+            if (GetComputerName(buffer, ref size))
+            {
+                string computerName = new string(buffer);
+                label1.Text += computerName;
+            }
+            else
+            {
+                Console.WriteLine("Не удалось получить название компьютера.");
+            }
+
+            size = 256; // Максимальная длина имени пользователя
+            StringBuilder buffer1 = new StringBuilder((int)size);
+
+            if (GetUserName(buffer1, ref size))
+            {
+                string userName = buffer1.ToString();
+                label2.Text += userName;
+            }
+            else
+            {
+                Console.WriteLine("Не удалось получить имя пользователя.");
+            }
+
+            size = 256; // Максимальная длина системной директории
+            StringBuilder buffer2 = new StringBuilder((int)size);
+
+            uint result = GetSystemDirectory(buffer2, size);
+
+            if (result != 0)
+            {
+                string systemDirectory = buffer2.ToString();
+                label3.Text += systemDirectory;
+            }
+            else
+            {
+                Console.WriteLine("Не удалось получить системную директорию.");
+            }
+
+            OSVERSIONINFOEX osVersionInfo = new OSVERSIONINFOEX();
+            osVersionInfo.dwOSVersionInfoSize = Marshal.SizeOf(typeof(OSVERSIONINFOEX));
+
+            if (GetVersionEx(ref osVersionInfo))
+            {
+                label4.Text += osVersionInfo.dwMajorVersion + "." + osVersionInfo.dwMinorVersion;
+            }
+
+            int cleanBootStatus = GetSystemMetrics(SM_CLEANBOOT);
+
+            if (cleanBootStatus == 1)
+            {
+                label5.Text += "Normal boot";
+            }
+
+            else
+            {
+                label5.Text += "Fail safe boot";
+            }
+
+            label6.Text += $"{Screen.PrimaryScreen.Bounds.Width} x {Screen.PrimaryScreen.Bounds.Height}";
+
+            label7.Text += GetPathOfWallpaper();
+
+            int contrast;
+            if (GetSystemContrast(out contrast))
+            {
+                label8.Text += $"{contrast}";
+            }
+            else
+            {
+                Console.WriteLine("Не удалось получить информацию о контрастности.");
+            }
+
+            int backgroundColor = GetSysColor(COLOR_BACKGROUND);
+
+            // Преобразуем значение цвета в RGB
+            int red = backgroundColor & 0xFF;
+            int green = (backgroundColor >> 8) & 0xFF;
+            int blue = (backgroundColor >> 16) & 0xFF;
+
+            label9.Text += $"[R = {red}, G = {green}, B = {blue}]";
+
+            DateTime currentTime = DateTime.Now;
+
+            CultureInfo russianCulture = new CultureInfo("ru-RU");
+
+            string formattedTime = currentTime.ToString("dd/MM/yyyy", russianCulture);
+
+            label10.Text += formattedTime;
+        }
+    }
+}
